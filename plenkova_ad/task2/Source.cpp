f8230fca9d31e638db31a63762c92d6431c65382
AnasteishA <nastya.plenkova@mail.ru>
2020-02-27 23:52:28 +0300

    task2

---
+++ plenkova_ad/task2/Source.cpp
@@ -0,0 +1,180 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+class String
+{
+private:
+	int   len;
+	char* str;
+public:
+	String()
+	{
+		str = new char[41];
+	}
+	~String()
+	{
+		delete[]str;
+	}
+	void SetString(char* word)
+	{
+		int len = strlen(word);
+		delete[]str;
+		str = new char[len + 1];
+		strcpy(str, word);
+	}
+	int GetLength()
+	{
+		return len;
+	}
+	bool Palindrom()
+	{
+		for (int i = 0; i < len / 2; i++)
+			if (str[i] != str[len - 1 - i])
+				return -1;
+		return 1;
+	}
+	String& operator=(String& obj1)
+	{
+		if (this == &obj1)
+			return *this;
+		else
+			if (len != obj1.len)
+			{
+				delete[] str;
+				str = new char[obj1.len + 1];
+			}
+		strcpy(str, obj1.str);
+		len = obj1.len;
+		return *this;
+	}
+	String(const String& obj2)
+	{
+		len = obj2.len;
+		str = new char[len + 1];
+		strcpy(str, obj2.str);
+	}
+	char GetSymbol(int index)
+	{
+		return str[index];
+	}
+	void ChangeSymbol(int index2, char change)
+	{
+		str[index2] = change;
+	}
+	char GetPartOfString(int k)
+	{
+		return str[k];
+	}
+	int DifferentSymbols()
+	{
+		int counter = 0;
+		int indicator;
+		for (int i = 0; i < len; i++)
+		{
+			indicator = 0;
+			for (int j = 0; j < i; j++)
+			{
+				if (str[i] != str[j])
+					if (isalpha(str[i]))
+						indicator++;
+			}
+			if (indicator == i)
+				counter++;
+		}
+		return counter;
+	}
+};
+int main()
+{
+
+	char word;
+	int start = 0;
+	int _length = 0;
+	int index = 0;
+	int h = 2;
+	int index2 = 0;
+	char change = 0;
+	int j = 0;
+	String S1;
+	cout << "Enter the string: ";
+	cin >> word;
+	S1.SetString(&word);
+	while (h == 2)
+	{
+		cout << "Set string - 1" << endl
+			<< "Get lenght of string - 2" << endl
+			<< "Get symbol - 3" << endl
+			<< "Change symbol - 4" << endl
+			<< "Get part of string - 5" << endl
+			<< "Check on palindrom - 6" << endl
+			<< "Different symbols - 7" << endl
+			<< "Exit - 0" << endl;
+		cin >> j;
+		switch (j)
+		{
+		case 1:
+		{
+			cout << "Enter the string: ";
+			cin >> word;
+			S1.SetString(&word);
+			break;
+		}
+		case 2:
+		{
+			cout << "\n" << S1.GetLength() << endl;
+			break;
+		}
+		case 3:
+		{
+			cout << "Enter index: ";
+			cin >> index;
+			cout << S1.GetSymbol(index) << endl;
+
+			break;
+		}
+		case 4:
+		{
+			cout << "\nEnter index: ";
+			cin >> index2;
+			cout << "\nEnter text: ";
+			cin >> change;
+			S1.ChangeSymbol(index2, change);
+			break;
+		}
+		case 5:
+		{
+			cout << "\n" << "Enter index: ";
+			cin >> start;
+			cout << "\n" << "Enter length: ";
+			cin >> _length;
+			for (int i = start; i < (start + _length); i++)
+			{
+				cout << S1.GetPartOfString(i);
+			}
+			cout << "\n";
+			break;
+		}
+		case 6:
+		{
+			if (S1.Palindrom() == 1)
+				cout << "String is a Palindrom" << endl;
+			else
+			{
+				cout << "String is not a Palindrom" << endl;
+			}
+			break;
+		}
+		case 7:
+		{
+			cout << "\n" << S1.DifferentSymbols() << endl;
+			break;
+		}
+		case 0:
+		{
+			h = 0;
+			break;
+		}
+		}
+	}
+};
